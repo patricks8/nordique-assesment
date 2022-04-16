ede099de4543c55980434fde2429c34246024476
Patrick Schiphouwer <patrick@qlic.nl>
2022-04-16 18:55:25 +0200

    Add visits to resource

--- app/Http/Resources/ShortUrlResource.php
+++ app/Http/Resources/ShortUrlResource.php
@@ -18,6 +18,7 @@ class ShortUrlResource extends JsonResource
             'id' => $this->id,
             'destination' => $this->destination,
             'url' => $this->url,
+            'visits' => $this->whenLoaded('visits'),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
             'deleted_at' => $this->deleted_at,
